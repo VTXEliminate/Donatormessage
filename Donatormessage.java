cd8583b7e473e658af93bc0175b2e918dab5ec6a
VTXEliminate <VTXEliminate@hotmail.com>
2013-11-14 00:29:30 +0100

    Create Donatormessage.java

---
+++ Donatormessage.java
@@ -0,0 +1,18 @@
+package io.github.VTXEliminate.Donatormessage;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Donatormessage extends JavaPlugin {
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+    	if(cmd.getName().equalsIgnoreCase("donate")){ // If the player typed /basic then do the following...
+    		sender.sendMessage("You can donate at: tinycash.buycraft.net");
+    		return true;
+    	} //If this has happened the function will return true. 
+            // If this hasn't happened the a value of false will be returned.
+    	return false; 
+    }
+    
+}
