4602e5c6536038c209177ade4494aea3ccf47382
VTXEliminate <VTXEliminate@hotmail.com>
2013-11-14 00:35:58 +0100

    Added /donor

--- Donatormessage.java
+++ Donatormessage.java
@@ -5,14 +5,19 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Donatormessage extends JavaPlugin {
+	
+	String website = "tinycash.buycraft.net";
     
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-    	if(cmd.getName().equalsIgnoreCase("donate")){ // /donate activates a line of text.
-    		sender.sendMessage("You can donate at: tinycash.buycraft.net");
+    	if(cmd.getName().equalsIgnoreCase("donate")){ // If the player typed /donate then do the following...
+    		sender.sendMessage("You can donate at: " + website);
     		return true;
-    	} //If this has happened the function will return true. 
-            // If this hasn't happened the a value of false will be returned.
-    	return false; 
-    }
+    	} else if(cmd.getName().equalsIgnoreCase("donor")){ // If the player typed /donor then do the following...
+    		sender.sendMessage("You can donate at: " + website);
+    		return true; //If this has happened the function will return true. 
+    	}
+    	
+    	return false;  // If this hasn't happened the a value of false will be returned.
     
+    }
 }
